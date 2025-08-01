5142501a539b8eaab3fa8d431d1bfb77613299b8
d-sribeiro <dalila.sribeiro@mercadolivre.com>
2025-08-01 19:16:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,8 @@ eps = eps_km / kms_per_radian
 db = DBSCAN(eps=eps, min_samples=7, algorithm='ball_tree', metric='haversine').fit(coords_hist_rad)
 
 # 5. Adicione os rótulos
-    coords_hist = coords_hist.copy()
-    coords_hist['cluster'] = db.labels_
+coords_hist = coords_hist.copy()
+coords_hist['cluster'] = db.labels_
 
 # 6. Junte os resultados
 hist_map = df_hist[["Shipment"]].merge(coords_hist[["Latitude", "Longitude", "cluster"]],
@@ -211,6 +211,7 @@ st.download_button(
 )
 
 #### FIM DA ALTERAÇÃO ------
+
 
 
 
